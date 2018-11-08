68de647eb9cc4bfef1e44f881b7d653790b3f184
Niall OByrnes <niallobyrnes@users.noreply.github.com>
2018-11-08 18:12:19 +0100

    Create nvidia.sh

---
+++ nvidia.sh
@@ -0,0 +1,28 @@
+#To Install cuda-10.0 and Jetpack-3.3 on Ubuntu 16.04
+#----------------------------------------------------
+
+sudo apt-get install linux-headers-$(uname -r)
+
+exit(0)
+#Check https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&target_distro=Ubuntu&target_version=1604&target_type=debnetwork
+
+sudo dpkg -i cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+sudo apt-get update
+sudo apt-get install cuda
+
+
+#The NVIDIA Persistence Daemon can be started as the root user by running:
+/usr/bin/nvidia-persistenced --verbose
+ 
+ 
+ 
+#Install Writable Samples
+cuda-install-samples-10.0.sh <dir>
+ 
+ 
+sudo apt-get install freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev
+ 
+ 
+ 
+ 
