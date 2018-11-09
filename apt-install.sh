ec0286d93f2d7ba83ae32b67c393257fa7eb1024
Niall OByrnes <niall@turbulentdynamics.com>
2018-11-09 18:29:33 +0100

    few changes

--- apt-install.sh
+++ apt-install.sh
@@ -1,6 +1,6 @@
 
 
-apt install vim git 
+apt install -y vim git curl 
 
 
 
@@ -11,7 +11,7 @@ apt install vim git
 cd ~
 git clone https://github.com/spack/spack.git
 
-
 echo "export SPACK_ROOT=~/spack">>~/.bashrc
 echo "export PATH=$SPACK_ROOT/bin:$PATH">>~/.bashrc
-
+echo ". $SPACK_ROOT/share/spack/setup-env.sh">>~/.bashrc
+spack bootstrap
---
+++ install-with-spack.sh
@@ -0,0 +1,9 @@
+
+
+spack install gcc@8.2.0 
+module load gcc@8.2.0
+spack compiler find
+
+
+
+spack install cmake@3.12.3%gcc@8.2.0 openmpi@3.1.2%gcc@8.2.0 openmpi@2.1.5%gcc@8.2.0 python@3.7.0%gcc@8.2.0 octave@4.2.1
